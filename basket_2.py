8d879e77f5d329d947146a85300116f9b2db7ab5
gdsrfg <riad52166@gmail.com>
2022-09-21 03:27:30 +0200

    sdf

--- basket_2.py
+++ basket_2.py
@@ -1,7 +1,8 @@
+'''
 from bs4 import BeautifulSoup as spp
 import requests
 import pandas
-
+#import xlsxwriter
 
 # Create a workbook and add a worksheet.
 
@@ -43,15 +44,14 @@ all_players = {}
 num_player = 0  
 
 num_player_print = 0
-for char in all_char:
+for char in all_char[0:3]:
     url = f'https://www.basketball-reference.com/players/{char}/'
     #print(char)
-    #name_file = f"all_players{char}"
+
     response_r = s.get(url)
     if response_r.status_code == 200:
-
         soup = spp(response_r.content,'html.parser')
- 
+
         count_players = soup.find('div',{'id':'all_players'}).find('h2').text.replace(' Players','')
         try :
             tag_players = soup.find('table',{'id':'players'}).find_all('th',{'data-stat':'player'})[1:]
@@ -72,17 +72,18 @@ for char in all_char:
         #print("Number Of Players page",char,len(tag_players))
     else:
         print("Internet Disconnected")
-    #print(count_players)
+    
+try:
 
-for player in all_players.values():
-    try:
+    for player in all_players.values():
         url_player = player['Link']
         response_r = s.get(url_player)
         if response_r.status_code == 200:
-            soup = spp(response_r.content,'html.parser')
-        print(url_player)
-    except:
-        print('no')
+           soup = spp(response_r.content,'html.parser')
+           print(player['Name'])
+except:
+
+
     ######
     #### Start Info Player
     ######
@@ -103,7 +104,11 @@ for player in all_players.values():
     except:
         name_player = "No Name"
         pass
+        
     all_p = info_player.find_all("p")
+
+            
+    print(name_of_player)
 
     #print("p",name_player)
     #print(all_p)
@@ -115,7 +120,7 @@ for player in all_players.values():
         if ("Position") in p_text:
             try:
                 player["Position"] = p_text.strip().replace("Position:","").replace("Shoots:","").replace("Right","").replace("▪","").replace("Left","").strip()
-
+                print(p_text)
             except:
                 player["Position"] = "No Position"
         #elif ("Shoots") in p_text:
@@ -173,11 +178,10 @@ for player in all_players.values():
 print(len(all_players))
 
 df = pandas.DataFrame.from_dict(all_players , orient="index",
-        columns=["Name",'Link','Position','Born_Date','Born_date','Born_place',
-        'NBA_Debut','College','High School','Height'])
+        columns=["Name",'Link','Position','Born_Date','Born_place',
+        'NBA Debut','College','High School','Height'])
 
-df.to_excel("file.xlsx",index=False)
-#df.to_excel(name_file+".xlsx",index=False)
+df.to_excel("all_players3.xlsx",index=False)
 
 
 #for player in all_players.values():    
@@ -188,4 +192,56 @@ df.to_excel("file.xlsx",index=False)
    
 
 print("################# Finish Info Player ################################################################")
+'''
+from bs4 import BeautifulSoup as soup
+import requests
 
+
+new_headersss = {
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.63"}
+
+base_url = "http://www.ufcstats.com/statistics/events/completed?page"
+s = requests.session()
+s.headers.update(new_headersss)
+n_range = 0
+
+all_productse = []
+link_productss = []
+link_2 = []
+for data in range(5):
+    n_range += 1
+    base_url = f"http://www.ufcstats.com/statistics/events/completed?page={n_range}"
+    r = s.get(base_url)
+    page = soup(r.content,"html.parser")
+    ufc = page.find_all('td',{'class':'b-statistics__table-col'})
+    
+    
+    all_productse.extend(ufc)
+for mob in all_productse:
+    try:
+      
+      link_product = mob.find('i',{'class':'b-statistics__table-content'}).find('a')['href']
+      
+      link_productss.append(link_product)
+
+    except:
+        pass
+
+for rr in link_productss:
+    r = s.get(rr)
+    page = soup(r.text,"html.parser")  
+    names = page.find('h2',{'class':'b-content__title'}).text
+    links2 = page.find('tbody',{'class':'b-fight-details__table-body'}).find('a')['href']
+    link_2.append(links2)
+    for dat in link_2:
+        r = s.get(dat)
+        page = soup(r.text,"html.parser")
+
+       
+        dtae = page.find('section',{'class':'b-statistics__section_details'}).text
+
+        W = page.find('i',{'class':'b-fight-details__person-status b-fight-details__person-status_style_green'})
+        L = page.find('i',{'class':'b-fight-details__person-status b-fight-details__person-status_style_gray'})
+        
+        for re in W:
+          print(re.text.strip())
